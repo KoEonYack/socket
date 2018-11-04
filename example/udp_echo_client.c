7e69fb9deeb328e31fb5cb248692db6b72599e2b
KoEonYack <21300035@handong.edu>
2018-11-04 21:56:17 +0900

    FIX BUG | chage udp echo client

--- example/udp_echo_client.c
+++ example/udp_echo_client.c
@@ -33,6 +33,22 @@ int main(int argc, char **argv)
     if(sock == -1) {
         error_handling("UDP 소켓 생성 오류");
     }
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family=AF_INET; 
+    serv_addr.sin_addr.s_addr=inet_addr(argv[1]); 
+    serv_addr.sin_port=htons(atoi(argv[2])); 
+    sendto(sock, MSG1, strlen(MSG1), 0, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); 
+    sendto(sock, MSG2, strlen(MSG2), 0, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); 
+    sendto(sock, MSG3, strlen(MSG3), 0, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    
+    for(i=0; i<3; i++) { 
+        addr_size = sizeof(from_addr); 
+        str_len = recvfrom(sock, message, BUFSIZE, 0, (struct sockaddr*)&from_addr, &addr_size);     
+        message[str_len]=0; printf("서버로부터 수신된 %d차 메시지 : %s \n", i, message); 
+    } 
+    
+    close(sock); 
     return 0;
 }
 
