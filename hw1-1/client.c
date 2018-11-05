eba5ba3864e138bec3eb95a12a3e0610f1deb12c
KoEonYack <21300035@handong.edu>
2018-11-05 23:09:50 +0900

    ADD | TCP socket

--- hw1-1/client.c
+++ hw1-1/client.c
@@ -1,3 +1,12 @@
+/**
+    * TCP [Client] Socket Programming 
+    * 
+    * 
+    * @ Auth          : KoEonyack, GyeowoonJeong
+    * @ Start Day     : 181105_2305
+    * @ Last Modified : 1181105_2305
+    * @ Link          : https://github.com/KoEonYack/socket/ (PRIVATE GIT)
+**/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -17,6 +26,12 @@ int main(int argc, char **argv)
     char message[BUFSIZE];
     int str_len;
     struct sockaddr_in serv_addr;
+
+    // Send 10 btye dummy message
+    char dummyMSG[] =  "0123456789"; 
+    char dummyMSG2[] = "ABCDEFGHIJ"; 
+    char dummyMSG3[] = "KLMNOPQRST";
+
     if(argc!=3){
         printf("Usage : %s <IP> <port>\n", argv[0]);
         exit(1);
@@ -32,14 +47,21 @@ int main(int argc, char **argv)
     serv_addr.sin_family=AF_INET;
     serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
     serv_addr.sin_port=htons(atoi(argv[2]));
-    if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1)
-    error_handling("connect() error!");
+
+    if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1){
+        error_handling("connect() error!");
+    }
+
+    write(sock, dummyMSG, strlen(dummyMSG + 1));
+    write(sock, dummyMSG2, strlen(dummyMSG2 + 1));
+    write(sock, dummyMSG3, strlen(dummyMSG3 + 1));
 
     while(1) {
         /* 메세지 입력, 전송 */
         fputs("전송할 메시지를 입력 하세요 (q to quit) : ", stdout);
         fgets(message, BUFSIZE, stdin);
         if(!strcmp(message,"q\n")) break;
+
         write(sock, message, strlen(message));
 
         /* 메세지 수신, 출력 */
