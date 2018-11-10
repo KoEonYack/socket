17465e766241946309796e6be8be0f69bc64966d
KoEonyack <21300035@handong.edu>
2018-11-10 12:50:49 +0900

    ADD | reference hw2-2 udp socket code

---
+++ hw2-2/client.c
@@ -0,0 +1,9 @@
+/**
+    * UDP [Client] Socket Programming 
+    * Data Sending.
+    * 
+    * @ Auth          : KoEonyack, GyeowoonJeong
+    * @ Start Day     : 181104_2215
+    * @ Last Modified : 181104_2215
+    * @ Link          : https://github.com/KoEonYack/socket/ (PRIVATE GIT)
+**/
---
+++ hw2-2/server.c
@@ -0,0 +1,11 @@
+/**
+    * UDP [Server] Socket Programming 
+    * 
+    * 
+    * @ Auth          : KoEonyack, GyeowoonJeong
+    * @ Start Day     : 181105_2305
+    * @ Last Modified : 1181105_2305
+    * @ Link          : https://github.com/KoEonYack/socket/ (PRIVATE GIT)
+    * @ Reference     : https://www.geeksforgeeks.org/c-program-for-file-transfer-using-udp/ 
+**/
+
